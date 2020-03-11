c7ba0d66eb328b292fd9be8b265e11f37ffb51de
brobroll <brobroll@outlook.jp>
2020-03-11 10:08:03 +0900

    リソース読み込みの構造体作った

--- BaseCross64/Karaage/GameSources/Scene.cpp
+++ BaseCross64/Karaage/GameSources/Scene.cpp
@@ -10,10 +10,53 @@
 namespace basecross{
 
 	//--------------------------------------------------------------------------------------
+	///	素材読み込み
+	//--------------------------------------------------------------------------------------
+	struct CreateResourse
+	{
+		//アセットディレクトリ
+		wstring dataDir;
+		//テクスチャ
+		wstring strTexture;
+		//音楽
+		wstring strSound;
+
+	public:
+		//テクスチャ読み込み
+		void SetTexture(wstring name, wstring filename)
+		{
+			strTexture = dataDir + L"Texture\\" + filename;
+			App::GetApp()->RegisterTexture(name, strTexture);
+		}
+		//音楽読み込み
+		void SetSound(wstring name, wstring filename)
+		{
+			strSound = dataDir + L"Sound\\" + filename;
+			App::GetApp()->RegisterWav(name, strSound);
+		}
+		//全リソース適用
+		void CreateResourses()
+		{
+			//ディレクトリ取得
+			App::GetApp()->GetAssetsDirectory(dataDir);
+
+			//テクスチャ
+			SetTexture(L"RANK_UI", L"Rank2.png");
+
+			//音楽
+
+		}
+	};
+
+	//--------------------------------------------------------------------------------------
 	///	ゲームシーン
 	//--------------------------------------------------------------------------------------
 	void Scene::OnCreate(){
 		try {
+			//リソース設定
+			CreateResourse resourse;
+			resourse.CreateResourses();
+
 			//クリアする色を設定
 			Col4 Col;
 			Col.set(31.0f / 255.0f, 30.0f / 255.0f, 71.0f / 255.0f, 255.0f / 255.0f);
