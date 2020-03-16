a031ae593abb6342d2c8f45ee170f062c8dac012
brobroll <brobroll@outlook.jp>
2020-03-16 14:04:50 +0900

    ワールド座標に変換して返す関数

--- BaseCross64/Karaage/GameSources/MouseManager.h
+++ BaseCross64/Karaage/GameSources/MouseManager.h
@@ -17,16 +17,17 @@ namespace basecross {
 
 	class MouseManager {
 	private:
+		//マウスポインタ
+		Point2D<int> m_mousePoint;
+		//NearとFar
+		perspective m_perspective;
 		//コンストラクタ
-		MouseManager() {};
+		MouseManager() : m_mousePoint(0,0) {};
 		//デストラクタ
 		~MouseManager() {};
 
 	public:
-		Vec3 GetMousePosition()const {
-		}
-
-		void GetMouseRay(Vec3& Near, Vec3& Far) {
+		perspective GetMouseRay() {
 			Mat4x4 world, view, proj;
 			world.affineTransformation(
 				Vec3(1.0f, 1.0f, 1.0f),
@@ -34,9 +35,39 @@ namespace basecross {
 				Vec3(0.0f, 0.0f, 0.0f),
 				Vec3(0.0f, 0.0f, 0.0f)
 			);
-
-			auto stage = App::
 			
+			auto ptrStage = App::GetApp()->GetScene<Scene>()->GetActiveStage();
+			auto ptrCamera = ptrStage->GetView()->GetTargetCamera();
+			view = ptrCamera->GetViewMatrix();
+			proj = ptrCamera->GetProjMatrix();
+			auto viewport = ptrStage->GetView()->GetTargetViewport();
+
+			m_perspective.Near = XMVector3Unproject(
+				Vec3((float)m_mousePoint.x, (float)m_mousePoint.y, 0),
+				viewport.TopLeftX,
+				viewport.TopLeftY,
+				viewport.Width,
+				viewport.Height,
+				viewport.MinDepth,
+				viewport.MaxDepth,
+				proj,
+				view,
+				world
+			);
+			m_perspective.Far = XMVector3Unproject(
+				Vec3((float)m_mousePoint.x, (float)m_mousePoint.y, 1.0),
+				viewport.TopLeftX,
+				viewport.TopLeftY,
+				viewport.Width,
+				viewport.Height,
+				viewport.MinDepth,
+				viewport.MaxDepth,
+				proj,
+				view,
+				world
+			);
+
+			return m_perspective;
 		}
 	};
 }
