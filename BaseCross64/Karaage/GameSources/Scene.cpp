a5efc01af644ee52173d228f68a2012f6f0370ba
brobroll <brobroll@outlook.jp>
2020-04-20 15:34:26 +0900

    セレクトステージ用のイベント

--- BaseCross64/Karaage/GameSources/Scene.cpp
+++ BaseCross64/Karaage/GameSources/Scene.cpp
@@ -82,7 +82,7 @@ namespace basecross{
 			SetClearColor(Col);
 			//自分自身にイベントを送る
 			//これにより各ステージやオブジェクトがCreate時にシーンにアクセスできる
-			PostEvent(0.0f, GetThis<ObjectInterface>(), GetThis<Scene>(), L"ToGameStage");
+			PostEvent(0.0f, GetThis<ObjectInterface>(), GetThis<Scene>(), L"ToSelectStage");
 		}
 		catch (...) {
 			throw;
@@ -96,6 +96,9 @@ namespace basecross{
 		if (event->m_MsgStr == L"ToGameStage") {
 			//最初のアクティブステージの設定
 			ResetActiveStage<GameStage>();
+		}
+		if (event->m_MsgStr == L"ToSelectStage") {
+			ResetActiveStage<SelectStage>();
 		}
 	}
 
